a22fb25b4d57d63d37a79d4bce75a86d8170cbc1
Stanislau Marudau <marudov.stas@gmail.com>
2024-01-24 18:04:12 +0200

    Installed TailWind and added helper styles.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import ErrorFallback from "./ui/ErrorFallback";
 
 import { ErrorBoundary } from "react-error-boundary";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary
-      FallbackComponent={ErrorFallback}
-      onReset={() => window.location.replace("/")}
-    ></ErrorBoundary>
+    <ErrorBoundary></ErrorBoundary>
     <App />
   </React.StrictMode>
 );
