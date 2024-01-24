9c481771e07cea9e4c6e80c97d292080e0b3cb8a
Stanislau Marudau <marudov.stas@gmail.com>
2024-01-24 18:49:00 +0200

    Added a component for Sign Up.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./GlobalStyles.css";
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <div className="flex h-screen">
       <Routes>
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
 
         <Route index path="/" element={<Home />} />
       </Routes>
