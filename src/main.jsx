29d3e594db0bf2343460b17e4c94960d08e418fc
Stanislau Marudau <marudov.stas@gmail.com>
2024-01-24 17:57:18 +0200

    Setup ESLint.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import ErrorFallback from "./ui/ErrorFallback";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ErrorBoundary } from "react-error-boundary";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    ></ErrorBoundary>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
