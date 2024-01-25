d016945a868499475f1c18815ab16675c982df1a
Stanislau Marudau <marudov.stas@gmail.com>
2024-01-25 14:22:32 +0200

    Setup the SignIn and SignUp components

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import FormRowVertical from "../ui/FormRowVertical";
+import Form from "../ui/Form";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (  <div>
+    <div className="flex-center flex-col sm:w-420">
+      <img src="../assets/images/logo.svg" alt="logo" />
+      <h2 className="h3-bold md:h2-bold pt-5 ">Create a new account</h2>
+      <p className="small-medium md:base-regular text-light-3">
+        To use SnapGram enter your details
+      </p>
+    </div>
+    <Form>
+    <FormRowVertical input="name" label="Name"></FormRowVertical>
+      <FormRowVertical input="username" label="Username"></FormRowVertical>
+      <FormRowVertical input="email" label="Email"></FormRowVertical>
+      <FormRowVertical input="password" label="Password"></FormRowVertical>
+      <button type="submit" className=" flex-center rounded-md px-3 py-2 shad-button_primary">Submit</button>
+    </Form>
+  </div>);
+
 };
 
 export default SignUp;
