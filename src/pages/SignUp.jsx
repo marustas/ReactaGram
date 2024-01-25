94ebbcc64e6aecdc76eb433a305b5b586565732d
Stanislau Marudau <marudov.stas@gmail.com>
2024-01-25 14:36:33 +0200

    Added a link to the sign-in form from the sign-up.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import FormRowVertical from "../ui/FormRowVertical";
 import Form from "../ui/Form";
 import Loader from "../ui/Loader";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const isLoading = true;
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async () => {
+    setIsLoading(true);
+
+    setIsLoading(false);
+  }
+
   return (  <div>
     <div className="flex-center flex-col sm:w-420">
       <img src="../assets/images/logo.svg" alt="logo" />
@@ -18,11 +25,15 @@ const SignUp = () => {
       <FormRowVertical input="username" label="Username"/>
       <FormRowVertical input="email" label="Email"/>
       <FormRowVertical input="password" label="Password"/>
-      <button type="submit" className=" flex-center rounded-md px-3 py-2 shad-button_primary">{
+      <button onSubmit={handleSubmit} type="submit" className=" flex-center rounded-md px-3 py-2 shad-button_primary">{
       isLoading ? <div className="flex-center gap-2">
         <Loader/>
         Loading...
         </div>  : "Sign Up"}</button>
+        <p className="text-small-regular text-light-2 text-center mt-2">
+          Already have an account?
+          <Link to="/sign-in" className="ml-1 text-primary-500 text-small-semibold text-primary">Log in</Link>
+          </p>
     </Form>
   </div>);
 
