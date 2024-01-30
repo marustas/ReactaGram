db9198a53705fa4a8f360b8ef3f324bce6ae00b1
Stanislau Marudau <marudov.stas@gmail.com>
2024-01-30 16:35:55 +0200

    Added react icons

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import AuthLayout from "./pages/AuthLayout";
-
+import toast, { ToastBar, Toaster } from "react-hot-toast";
+import { RxCross2 } from "react-icons/rx";
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 0 } },
 });
@@ -22,6 +23,38 @@ const App = () => {
         <Route index path="/" element={<Home />} />
       </Routes>
     </div>
+    <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: { duration: 3000 },
+            error: { duration: 5000 },
+            style: {
+              fontSize: "20px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-0)",
+              color: "var(--color-grey-700)",
+            },
+          }}
+        >
+          {(t) => (
+            <ToastBar toast={t}>
+              {({ icon, message }) => (
+                <>
+                  {icon}
+                  {message}
+                  {t.type !== "loading" && (
+                    <CloseToastButton onClick={() => toast.dismiss(t.id)}>
+                      <RxCross2 />
+                    </CloseToastButton>
+                  )}
+                </>
+              )}
+            </ToastBar>
+          )}
+        </Toaster>
     </QueryClientProvider>
   );
 };
