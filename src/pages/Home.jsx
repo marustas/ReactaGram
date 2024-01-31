4077a7a602ca922e8116194d4967b9a80476543c
Stanislau Marudau <marudov.stas@gmail.com>
2024-01-31 19:38:46 +0200

    Added the architecture for the main layout.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
+import {Outlet} from "react-router-dom";
+
 import React from "react";
+import TopBar from "./TopBar";
+import LeftBar from "./LeftBar";
+import BottomBar from "./BottomBar";
 
 const Home = () => {
-  return <div>Dashboard</div>;
+  return <div className="w-full md:flex">
+    <TopBar/>
+    <LeftBar/>
+    <section className="flex-1 h- full flex">
+      <Outlet/>
+    </section>
+    <BottomBar/>
+  </div>;
 };
 
 export default Home;
