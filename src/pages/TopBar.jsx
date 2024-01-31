0927f945408ae9ba5252375ed0751fdf52b45c6f
Stanislau Marudau <marudov.stas@gmail.com>
2024-01-31 21:03:49 +0200

    Fixed indetation in styles and layed the foundation of the topbar

--- src/pages/TopBar.jsx
+++ src/pages/TopBar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const TopBar = () => {
   return (
-    <div>TopBar</div>
+    <section className='topbar'>
+        <div className='flex-between py-4 px-5'>
+            <Link to="/" className='items-center flex gap-3'>
+                <img height={325} width={130} alt = "logo" src = "../assets/images/logo.svg"/>
+            </Link>
+            <div className='flex gap-4'>
+              <button className='shad-button_ghost'>
+              <img height={325} width={130} alt = "logout" src = "../assets/icons/logout.svg"/>
+              </button>
+            </div>
+        </div>
+    </section>
   )
 }
 
