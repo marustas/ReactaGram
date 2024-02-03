9ad99a28ce1cb7aaa71a53045ef2fa663afc4552
Stanislau Marudau <marudov.stas@gmail.com>
2024-02-03 11:50:06 +0200

    Added styling and icons to the sidebar.

--- src/ui/LinkList.jsx
+++ src/ui/LinkList.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom'
 const LinkList = ({links}) => {
   return (
     <ul>
-        {links.map((link)=><li key={link.label}>
-            <Link className='leftsidebar-link' to = {link.route}>{link.label}</Link>
+        {links.map((link)=><li className='leftsidebar-link' key={link.label}>
+            <Link className='flex gap-4 items-center p-4'  to = {link.route}>
+              <img className='group-hover:invert-white' src = {link.imgURL} alt = {link.label}/>
+              {link.label}
+            </Link>
         </li>)}
     </ul>
   )
