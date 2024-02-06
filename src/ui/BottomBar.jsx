4eba975a31542b497cafaeb3ebda8c558aa4c349
Stanislau Marudau <marudov.stas@gmail.com>
2024-02-06 19:11:06 +0200

    Finished teh stylization of the Bottom bar

--- src/ui/BottomBar.jsx
+++ src/ui/BottomBar.jsx
@@ -9,12 +9,10 @@ const BottomBar = () => {
       {bottomBarLinks.map((link) => {
         const isActive = pathname === link.route;
         return(
-          <li className={`bottombar-link group ${isActive && 'bg-primary-500'}`} key={link.label}>
-            <Link className='flex gap-4 items-center p-4'  to = {link.route}>
-            <img className={`group-hover:invert-white ${isActive && 'invert-white'}`} src = {link.imgURL} alt = {link.label}/>
-          {link.label}
-            </Link>
-          </li>
+          <Link className={`${isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col gap-1 p-2 transition`} key={link.label}  to = {link.route}>
+            <img width={16} height={16} className={`${isActive && 'invert-white'}`} src = {link.imgURL} alt = {link.label}/>
+            <p className='tiny-medium text-light-2'>{link.label}</p>
+        </Link>
         )
       })}
     </section>
