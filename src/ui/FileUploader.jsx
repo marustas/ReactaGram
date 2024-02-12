3f94c038510f8bd3ef46693ccd53e8347d27e282
Stanislau Marudau <marudov.stas@gmail.com>
2024-02-12 17:35:56 +0200

    Implemented the UI for the FileUploader

--- src/ui/FileUploader.jsx
+++ src/ui/FileUploader.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
 
 const FileUploader = () => {
+  const [fileUrl, setFileUrl] = useState('');
+
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
   return (
-    <div>FileUploader</div>
+    <div className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer' {...getRootProps()}>
+      <input className='cursor-pointer' {...getInputProps()} />
+      {
+        fileUrl ? (
+           <div>
+
+           </div> ) : (
+           <div className='file_uploader-box'>
+            <img width={96} height={77} alt="upload-icon" src='../assets/icons/file-upload.svg'/>
+            <h3 className='base-medium mb-2 mt-6 text-light-2'>Drag photo here</h3>
+            <p className='text-light-4 small-regular mb-6'>SVG, PNG, JPG</p>
+            <button className='items-center rounded-md shad-button_dark_4'>Select from computer</button>
+           </div> )
+      }
+    </div>
   )
 }
 
--- src/ui/PostForm.jsx
+++ src/ui/PostForm.jsx
@@ -9,16 +9,16 @@ const PostForm = () => {
         <textarea className='px-3 py-2 rounded-md shad-textarea custom-scrollbar' placeholder='Caption'/>
       </div>
       <div className='flex flex-col gap-2 py-1.5'>
-        <label className='shad-form_label'>Add location</label>
-        <input type="text" className='px-3 py-2 rounded-md shad-input' placeholder='Location'/>
-      </div>
-      <div className='flex flex-col gap-2 py-1.5'>
         <label className='shad-form_label'>Add Tags (separated by comma " , ")</label>
         <input type="text" className='px-3 py-2 rounded-md shad-input' placeholder='Sports, Arts, History'/>
       </div>
       <div className='flex flex-col gap-2 py-1.5'>
         <label>Post image</label>
         <FileUploader/>
+      </div>
+      <div className='flex flex-col gap-2 py-1.5'>
+        <label className='shad-form_label'>Add location</label>
+        <input type="text" className='px-3 py-2 rounded-md shad-input' placeholder='Location'/>
       </div>
       <div className='flex gap-4 items-center justify-end'>
         <button className='p-3 rounded-md shad-button_dark_4'>Cancel</button>
