85767e521d56251b0bc29073a7192bc1b15d7fba
Stanislau Marudau <marudov.stas@gmail.com>
2024-02-19 22:59:31 +0200

    Added the styling for the PostCard component and wrote comments for future implementation  guidance

--- src/ui/LeftBar.jsx
+++ src/ui/LeftBar.jsx
@@ -16,7 +16,7 @@ const LeftBar = () => {
         <Link to="/" className='items-center flex gap-3'>
           <img height={36} width={170} alt = "home" src = "../assets/images/logo.svg"/>
         </Link>
-        <Link to={`/profile/id:${user.id}`} className='items-center flex gap-3'>
+        <Link to={`/profile/:${user.id}`} className='items-center flex gap-3'>
           <img className='h-14 w-14 rounded-full' alt = "profile" src = "../assets/images/profile.png"/>
           <div className='flex flex-col'>
           <p className='body-bold'>{username}</p>
--- src/ui/PostCard.jsx
+++ src/ui/PostCard.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PostCard = ({post}) => {
+    //Need to add a userId do the post data object and supabase
   return (
-    <li>PostCard</li>
+    <li className='post-card'>
+        <div className='flex-between'>
+            <div className='flex items-center gap-3'>
+                <Link to={`/profile/${post.username}`}>
+                    <img className='rounded-full w-12 lg:h-12' alt='creator' src = {post.mediaUrl} />
+                </Link>
+                <div className='flex flex-col'>
+                    <p className='base-medium lg:body-bold text-light-1'>{post.username}</p>
+                    <div className='flex-center text-light-3 gap-2'>
+                        <p className='subtle-semibold lg:small-regular'>{post.created_at}</p>
+                        -
+                        <p className='subtle-semibold lg:small-regular'>{post.location}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </li>
   )
 }
 
