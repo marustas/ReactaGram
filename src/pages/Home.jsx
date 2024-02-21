9bee579ec2d71b09fb98b9bcb39f938130e7a270
Stanislau Marudau <marudov.stas@gmail.com>
2024-02-21 17:10:56 +0200

    Cleanup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Loader from "../ui/Loader"
+
 import { useLoadPosts } from "../hooks/useLoadPosts";
+import Loader from "../ui/Loader"
 import PostCard from "../ui/PostCard";
 
 const Home = () => {
@@ -12,7 +13,7 @@ const Home = () => {
           <h2 className="h3-bold md:h2-bold text-left w-full">Some Feed</h2>
           {isPostLoading && !posts ? <Loader/> : (
           <ul className="flex flex-col gap-9 w-full flex-1">
-            {posts.map((post)=> <PostCard key={post.id} post = {post}/>)}
+            {posts.map((post)=> <PostCard key = {post.id} post = {post}/>)}
           </ul> 
             )}
         </div>
