c70f4ba2bd09b025ce3a5c6fad6712bd4ee2a107
Stanislau Marudau <marudov.stas@gmail.com>
2024-02-24 13:35:03 +0200

    Finished the UI for PostStats.

--- src/ui/PostStats.jsx
+++ src/ui/PostStats.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const PostStats = () => {
+const PostStats = ({post}) => {
   return (
-    <div>PostStats</div>
+    <div className='flex justify-between z-20 items-center'>
+        <div className='flex gap-2 mr-5'>
+            <img className='cursor-pointer' onClick={()=>{}} src='..assets/icons/liked.svg' alt='like' width={20} height={20}/>
+            <p className='small-medium lb:base-medium'>{post.likes}</p>
+        </div>
+        <div className='flex gap-2'>
+            <img className='cursor-pointer' onClick={()=>{}} src='..assets/icons/saved.svg' alt='like' width={20} height={20}/>
+        </div>
+    </div>
   )
 }
 
