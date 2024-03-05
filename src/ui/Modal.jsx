7a99b768c74b946b4dd36d80c86e8b96ff792117
Stanislau Marudau <marudov.stas@gmail.com>
2024-03-05 12:20:43 +0200

    Created a Compound Component for Modal Window

---
+++ src/ui/Modal.jsx
@@ -0,0 +1,39 @@
+import React, { cloneElement, createContext, useContext, useState } from 'react'
+
+const ModalContext = createContext();
+
+const Modal = ({children}) => {
+
+    const [openName, setOpenName] = useState('');
+    const close = ()=>setOpenName('');
+    const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{openName, open, close}}>
+        {children}
+    </ModalContext.Provider>
+  )
+}
+
+const Open = ({children, opens: opensWindowName})=>{
+    const {open} = useContext(ModalContext);
+    return cloneElement(children, {onClick: ()=> open(opensWindowName)})
+}
+
+const Window = ({children, name})=>{
+    const {close, openName} = useContext(ModalContext);
+
+    if(name !== openName) return null;
+
+    return (
+    <div className='fixed top-[50%] left-[50%] rounded-md px-5 py-8'>
+        <button onClick={close}>X</button>
+        {children}
+    </div> 
+    )
+}
+
+Modal.Open = Open;
+Modal.Window = Window;
+
+export default Modal
