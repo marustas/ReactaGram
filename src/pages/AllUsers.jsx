a9eb32d0b3522de3e7af897a7252b63a94518ca9
Stanislau Marudau <marudov.stas@gmail.com>
2024-03-10 20:39:11 +0200

    Added a skeleton for component for list of users

--- src/pages/AllUsers.jsx
+++ src/pages/AllUsers.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Loader from '../ui/Loader';
+import UsersList from '../ui/UsersList';
 
 const AllUsers = () => {
+  const {users, isLoading} = useUsers();
   return (
     <div className='user-container'>
       <h2 className='h3-bold md:h2-bold w-full'>All users</h2>
+      {isLoading ? <Loader/> : <UsersList users = {users}/>}
     </div>
   )
 }
---
+++ src/ui/UsersList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UsersList = ({users}) => {
+  return (
+    <div className='user-grid'>UsersList</div>
+  )
+}
+
+export default UsersList
