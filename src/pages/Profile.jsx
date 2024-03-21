b9d15c8d6ceeaed6ecf21e90fca624e448941c48
Stanislau Marudau <marudov.stas@gmail.com>
2024-03-21 17:41:03 +0200

    Finished the markup for the profile picture, name and username

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,12 +9,28 @@ const Profile = () => {
   return (
     <div className='profile-container'>
       {isLoading ? <Loader/> : 
-      <div className='profile_inner-container'>
-        <img src = {user.profileImage || "../assets/icons/profile-placeholder.svg"} alt='profile' />
-        <h2 className='h3-bold md:h2-bold w-full'>{user.name}</h2>
-        <p className='small-regular text-light-3'>@{user.username}</p>
+      <div className="profile-inner_container">
+        <div className="flex xl:flex-row flex-col max-xl:items-center flex-1 gap-7">
+          <img
+            src={
+              user.profileImage || "../assets/icons/profile-placeholder.svg"
+            }
+            alt="profile"
+            className="w-28 h-28 lg:h-36 lg:w-36 rounded-full"
+          />
+          <div className="flex flex-col flex-1 justify-between md:mt-2">
+            <div className="flex flex-col w-full">
+              <h1 className="text-center xl:text-left h3-bold md:h1-semibold w-full">
+                {user.name}
+              </h1>
+              <p className="small-regular md:body-medium text-light-3 text-center xl:text-left">
+                @{user.username}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>}
-    </div>
+  </div>
   )
 }
 
