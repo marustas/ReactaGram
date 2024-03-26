1fb3b509cf564c054220436435da00882cdd77c1
Stanislau Marudau <marudov.stas@gmail.com>
2024-03-26 20:29:42 +0200

    Consumed the API and added to update user form

--- src/hooks/useUpdateUser.jsx
+++ src/hooks/useUpdateUser.jsx
@@ -1,3 +1,15 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateUser as updateUserApi } from "../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
 export function useUpdateUser(){
-    
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+    const {isLoading: isUpdatingUser, mutate: updateUser} = useMutation({
+        mutationFn: updateUserApi,
+        onSuccess: () => { navigate('/'); queryClient.invalidateQueries({queryKey: ['currentUser']})},
+        onError: (error) => {toast.error(error.message)}
+    })
+    return {isUpdatingUser, updateUser};
 }
--- src/ui/ProfileForm.jsx
+++ src/ui/ProfileForm.jsx
@@ -9,7 +9,7 @@ const ProfileForm = ({profile}) => {
     const navigate = useNavigate();
     const {handleSubmit, register } = useForm();
     const {profileImage, setProfileImage} = useState(null);
-    const {isUpdating, updateUser} = useUpdateUser();
+    const {isUpdatingUser, updateUser} = useUpdateUser();
     
     function onSubmit({username, name, bio, }){
       updateUser({name: name,
@@ -24,11 +24,11 @@ const ProfileForm = ({profile}) => {
     <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-9 w-full max-w-5xl'>
         <div className='flex flex-col gap-2 py-1.5'>
           <label className='shad-form_label'>Name</label>
-          <textarea disabled={isUpdating} {...register("name")} className='px-3 py-2 rounded-md shad-textarea custom-scrollbar' placeholder= { profile?.name}/>
+          <textarea disabled={isUpdatingUser} {...register("name")} className='px-3 py-2 rounded-md shad-textarea custom-scrollbar' placeholder= { profile?.name}/>
         </div>
         <div className='flex flex-col gap-2 py-1.5'>
           <label className='shad-form_label'>Username</label>
-          <input disabled={isUpdating}  {...register("username")} type="text" className='px-3 py-2 rounded-md shad-input' placeholder = { profile?.username}/>
+          <input disabled={isUpdatingUser}  {...register("username")} type="text" className='px-3 py-2 rounded-md shad-input' placeholder = { profile?.username}/>
         </div>
         <div className='flex flex-col gap-2 py-1.5'>
           <label>Profile image</label>
@@ -36,7 +36,7 @@ const ProfileForm = ({profile}) => {
         </div>
         <div className='flex flex-col gap-2 py-1.5'>
           <label className='shad-form_label'>Bio</label>
-          <input disabled={isUpdating}  {...register("bio")} type="text" className='px-3 py-2 rounded-md shad-input' placeholder={ profile?.bio}/>
+          <input disabled={isUpdatingUser}  {...register("bio")} type="text" className='px-3 py-2 rounded-md shad-input' placeholder={ profile?.bio}/>
         </div>
         <div className='flex gap-4 items-center justify-end'>
           <button onClick={()=>navigate(-1)} className='p-3 rounded-md shad-button_dark_4'>Cancel</button>
